ae65fe63edde8ebfe8fc201aed98249b6af9a8e2
Anran <qina@sheridancollege.ca>
2019-09-15 22:27:13 -0400

    dynamically add InputFragment onto activity_main

--- app/src/main/java/sheridan/qina/assignment1/MainActivity.java
+++ app/src/main/java/sheridan/qina/assignment1/MainActivity.java
@@ -1,14 +1,26 @@
 package sheridan.qina.assignment1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.FragmentManager;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements InputFragment.InputListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            InputFragment inputFragment = InputFragment.newInstance();
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.fragment_placeholder, inputFragment).commit();
+        }
+
+    }
+
+    @Override
+    public void updateMessage(String text) {
+
     }
 }
